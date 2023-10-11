5d2b0498bb317c4aa7a1431c8f1644bc62dba202
Donsidious <137007350+Donsidious@users.noreply.github.com>
2023-10-10 20:55:40 -0600

    commit

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../../SportsFans/Develop/models');
+const { User } = require('../../models/User');
 
 router.post('/', async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -29,5 +29,3 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
-
-keep
